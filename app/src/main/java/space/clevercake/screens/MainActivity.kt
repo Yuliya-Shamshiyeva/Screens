6de8a48429a7d038e92c4678db76674fd48eb04c
unknown <clevercake.web@gmail.com>
2024-02-19 19:26:12 +0600

    Initial commit

---
+++ app/src/main/java/space/clevercake/screens/MainActivity.kt
@@ -0,0 +1,11 @@
+package space.clevercake.screens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.screen3)
+    }
+}
